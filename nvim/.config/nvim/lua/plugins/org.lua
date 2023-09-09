3af36a88f9e42154fb2addce230f81f90af7c538
Maanav1112 <anugrahanmaanav@gmail.com>
2023-09-09 11:54:59 +0530

    Commit on 9/9/23

--- nvim/.config/nvim/lua/plugins/org.lua
+++
@@ -1,7 +0,0 @@
--- Load custom treesitter grammar for org filetype
-require('orgmode').setup_ts_grammar()
-
-require('orgmode').setup({
-  org_agenda_files = {'~/notes/*', '~/notes/**/*'},
-  org_default_notes_file = '~/notes/refile.org',
-})
